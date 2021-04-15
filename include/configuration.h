dabb1539a4cce84c7f5720480fcb50bacb4baf3f
eoa-electronics <webmaster@eoa-electronics.de>
2021-04-15 13:03:37 +0200

    written main program

--- include/configuration.h
+++ include/configuration.h
@@ -3,40 +3,40 @@
 
 //**COMMANDS**
 //This is the commands section. Here you can define, which keys have to pressed for the different functions.
-//Alphanumerical keys are written as numbers(1) or lowercase letters(a)
-//The same applies to common symbols like ; - * etc.
+//Alphanumerical keys are written as numbers('1') or lowercase letters in single quotation marks('a')
+//The same applies to common symbols like ; - * etc.(';')
 //Special keys and modifier-keys are written as key-codes(KEY_LEFT_CTRL)
 //You can find all the key-codes at: https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers/
 //Each function can have up to 3 keys pressed simultaneously but doesn't have to
-//The second an thitd key can be disabled for each function
+//The second an third key can be disabled for each function
 
 //Command: Mute
 #define MUTE_KEY_1 KEY_LEFT_CTRL
 #define MUTE_EN_2 true
 #define MUTE_KEY_2 KEY_LEFT_SHIFT
 #define MUTE_EN_3 true
-#define MUTE_KEY_3 m
+#define MUTE_KEY_3 'm'
 
 //Command: Un-Mute
 #define UNMUTE_KEY_1 KEY_LEFT_CTRL
 #define UNMUTE_EN_2 true
 #define UNMUTE_KEY_2 KEY_LEFT_SHIFT
 #define UNMUTE_EN_3 true
-#define UNMUTE_KEY_3 m
+#define UNMUTE_KEY_3 'm'
 
 //Command Raise-Hand
 #define RAISE_KEY_1 KEY_LEFT_CTRL
 #define RAISE_EN_2 true
 #define RAISE_KEY_2 KEY_LEFT_SHIFT
 #define RAISE_EN_3 true
-#define RAISE_KEY_3 k
+#define RAISE_KEY_3 'k'
 
 //Command Un-Raise-Hand
 #define UNRAISE_KEY_1 KEY_LEFT_CTRL
 #define UNRAISE_EN_2 true
 #define UNRAISE_KEY_2 KEY_LEFT_SHIFT
 #define UNRAISE_EN_3 true
-#define UNRAISE_KEY_3 k
+#define UNRAISE_KEY_3 'k'
 
 
 //**BUTTONS**
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include "configuration.h"
 #include "pins.h"
 
+bool isMute = true;
+bool isRaise = false;
+
 void pinModeOutOff(int pin) {
   pinMode(pin, OUTPUT);
   digitalWrite(pin, LOW);
@@ -17,6 +20,43 @@ void ledWrite(int led, bool state, bool en, bool inv) {
       digitalWrite(led, !state);
     }
   }
+}
+
+bool btRead(int bt, bool en, bool inv) {
+  bool t1 = false;
+  bool t2 = false;
+  if(en) {
+    if(!inv) {
+      t1 = !digitalRead(bt);
+    } else {
+      t1 = digitalRead(bt);
+    }
+    delay(50);
+    if(!inv) {
+      t2 = !digitalRead(bt);
+    } else {
+      t2 = digitalRead(bt);
+    }
+    if(t1 == LOW || t2 == LOW) {
+      return(LOW);
+    } else {
+      return(HIGH);
+    }
+  } else {
+    return(LOW);
+  }
+}
+
+void pressCombination(char k1, bool k2en, char k2, bool k3en, char k3) {
+  Keyboard.press(k1);
+  if(k2en) {
+    Keyboard.press(k2);
+  }
+  if(k3en) {
+    Keyboard.press(k3);
+  }
+  delay(100);
+  Keyboard.releaseAll();
 }
 
 
@@ -33,5 +73,30 @@ void setup() {
 }
 
 void loop() {
-  
+  if(btRead(PIN_BT_MUTE, BT_MUTE_EN, BT_MUTE_INV)) {
+    if(isMute) {
+      ledWrite(PIN_LED_MUTE_OFF, HIGH, LED_MUTE_OFF_EN, LED_MUTE_OFF_INV);
+      ledWrite(PIN_LED_MUTE_ON, LOW, LED_MUTE_ON_EN, LED_MUTE_ON_INV);
+      pressCombination(UNMUTE_KEY_1, UNMUTE_EN_2, UNMUTE_KEY_2, UNMUTE_EN_3, UNMUTE_KEY_3);
+      isMute = false;
+    } else {
+      ledWrite(PIN_LED_MUTE_OFF, LOW, LED_MUTE_OFF_EN, LED_MUTE_OFF_INV);
+      ledWrite(PIN_LED_MUTE_OFF, HIGH, LED_MUTE_ON_EN, LED_MUTE_ON_INV);
+      pressCombination(MUTE_KEY_1, MUTE_EN_2, MUTE_KEY_2, MUTE_EN_3, MUTE_KEY_3);
+      isMute = true;
+    }
+  }
+  if(btRead(PIN_BT_RAISE, BT_RAISE_EN, BT_RAISE_INV)) {
+    if(!isRaise) {
+      ledWrite(PIN_LED_RAISE_ON, HIGH, LED_RAISE_ON_EN, LED_RAISE_ON_INV);
+      ledWrite(PIN_LED_RAISE_OFF, LOW, LED_RAISE_OFF_EN, LED_RAISE_OFF_INV);
+      pressCombination(RAISE_KEY_1, RAISE_EN_2, RAISE_KEY_2, RAISE_EN_3, RAISE_KEY_3);
+      isRaise = true;
+    } else {
+      ledWrite(PIN_LED_RAISE_ON, LOW, LED_RAISE_ON_EN, LED_RAISE_ON_INV);
+      ledWrite(PIN_LED_RAISE_OFF, HIGH, LED_RAISE_OFF_EN, LED_RAISE_OFF_INV);
+      pressCombination(UNRAISE_KEY_1, UNRAISE_EN_2, UNRAISE_KEY_2, UNRAISE_EN_3, UNRAISE_KEY_3);
+      isRaise = false;
+    }
+  }
 }
